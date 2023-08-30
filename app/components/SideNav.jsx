f6bfdad7bcb627807c1908e3e4fb37d1d63bc3d8
Ivar <jeluzadrian07@gmail.com>
2023-08-30 13:46:06 +0800

    Made improvements to the code structure of sidenav
    These changes made prepares the code for the mobile navbar

--- app/components/SideNav.jsx
+++ app/components/SideNav.jsx
@@ -1,76 +1,103 @@
 "use client"
-import Link from 'next/link';
 
 import { useState } from 'react';
-import { usePathname } from 'next/navigation';
-import { useRouter } from 'next/navigation';
+import SideBarIcon from './ui/SideBarIcon';
 import { HomeIcon, RibbonIcon, CalendarIcon, 
   DebitCardIcon, DropboxIcon, FolderIcon, 
   StudentIcon, UserIcon, SettingsIcon,
   MegaphoneIcon, ExitIcon, ArrowIcon } from '@/public/assets/icons';
 
+
 export default function SideNav() {
   const [isCollpased, setCollapsed] = useState(false);
-  const router = useRouter();
-  const pathname = usePathname();
 
-  const SideBarIcon = ({icon,label,link}) => (
-    
-    <Link href={link}>
-        <div className={`sidenav-icon text-[32px] ${pathname === link? 'active-link': ''}`}>
-            {icon} 
-            <span className='nav-icon-label'>
-                {label}
-            </span> 
-            {/* {!isCollpased && (
-              
-            )} */}
-        </div>
-    </Link>
-  );
+  const portalLinks = [{
+    path: '/',
+    icon: <HomeIcon />,
+    label: 'home',
+  },{
+    path: '/grades',
+    icon: <RibbonIcon />,
+    label: 'grades',
+  },{
+    path: '/schedule',
+    icon: <CalendarIcon />,
+    label: 'schedule',
+  },{
+    path: '/payments',
+    icon: <DebitCardIcon />,
+    label: 'payments',
+  },{
+    path: '/requests',
+    icon: <DropboxIcon />,
+    label: 'requests',
+  },{
+    path: '/documents',
+    icon: <FolderIcon />,
+    label: 'documents',
+  },{
+    path: '/student-info',
+    icon: <StudentIcon />,
+    label: 'student information',
+  }]
+
+
   
   return (
-    <div className={`side-nav group/container ${isCollpased ? 'nav-collapsed': ''}`}>
-      <button 
-      className={`top-[64px] right-[-16px] absolute 
-      w-8 h-8 text-white text-2xl
-      rounded-3xl bg-darkGray
-      ${isCollpased? 'rotate-0' : 'rotate-180' }
-      `}
-      onClick={()=>{
-        setCollapsed(prev => (!prev));
-      }}>
-        <ArrowIcon className="mx-auto" />
-      </button>
 
-      <div className="portalLinks">
-        <SideBarIcon icon={<HomeIcon />} label="Home" link="/"/>
-        <SideBarIcon icon={<RibbonIcon />} label="Grades" link="/grades"/>
-        <SideBarIcon icon={<CalendarIcon />} label="Schedule" link="/schedule"/>
-        <SideBarIcon icon={<DebitCardIcon />} label="payments" link="/payments"/>
-        <SideBarIcon icon={<DropboxIcon />} label="requests" link="/requests"/>
-        <SideBarIcon icon={<FolderIcon />} label="documents" link="/documents"/>
-        <SideBarIcon icon={<StudentIcon />} label="student information" link="/student-info"/>
-      </div>
-      <div className="accountLinks">
-        <SideBarIcon icon={<UserIcon />} label="profile" link="/profile"/>
-        <SideBarIcon icon={<SettingsIcon />} label="settings" link="/settings"/>
-        <SideBarIcon icon={<MegaphoneIcon />} label="announcements" link="/announements" />
-          
-        <div className="
-        flex items-center gap-4
-        p-3 w-auto mx-5 rounded-md text-gray text-[32px] cursor-pointer
-        group-[.nav-collapsed]/container:w-14
-        ">
-          <ExitIcon />
-          {!isCollpased && (
+    //* Desktop Navigation
+
+    <>
+      <div className={`side-nav group/container ${isCollpased ? 'nav-collapsed': ''}`}>
+        <button 
+        className={`top-[64px] right-[-16px] absolute 
+        w-8 h-8 text-white text-2xl
+        rounded-3xl bg-darkGray
+        ${isCollpased? 'rotate-0' : 'rotate-180' }
+        `}
+        onClick={()=>{
+          setCollapsed(prev => (!prev));
+        }}>
+          <ArrowIcon className="mx-auto" />
+        </button>
+      
+        <div className="portalLinks">
+          <h2 className="text-primary text-2xl font-extrabold mx-auto my-4">
+            ACTEC
+          </h2>
+          {portalLinks.map((link) =>{
+            return(
+              <SideBarIcon key={link.path} icon={link.icon} label={link.label} link={link.path}/>
+            )
+          })}
+        </div>
+        <div className="accountLinks">
+          <SideBarIcon icon={<UserIcon />} label="profile" link="/profile"/>
+          <SideBarIcon icon={<SettingsIcon />} label="settings" link="/settings"/>
+          <SideBarIcon icon={<MegaphoneIcon />} label="announcements" link="/announements" />
+            
+          <div className="
+          flex items-center gap-4
+          p-3 w-auto mx-5 rounded-md text-gray text-[32px] cursor-pointer
+          group-[.nav-collapsed]/container:w-14
+          ">
+            <ExitIcon />
               <span className='nav-icon-label text-black'>
                 Logout
               </span>
-            )}
+          </div>
         </div>
       </div>
-    </div>
+
+      
+      {/* 
+        Mobile Navigation
+      */}
+      <div className="mobileNav">
+        
+      </div>
+    </>
+  
   )
 }
 
