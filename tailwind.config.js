41e92b479284e8879e994f2d6c19bc67915f249f
Ivar <jeluzadrian07@gmail.com>
2023-08-30 11:50:31 +0800

    Added custom colors in tailwind config.
    These colors are based on the figma prototype project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,21 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        primary: '#1473E6',
+        lightPrimary: '#AACDF8',
+        transparentPrimary: 'rgba(20,115,230,0.2)',
+        secondary: '#1D5599',
+        darkGreen:'#70B309',
+        lightGreen: '#A5FF19',
+        warning: '#FFB904',
+        error: '#E63D2C',
+        errorVariant: 'FF4419',
+        gray: '#8C8CA1',
+        plat: '#e6e6e6',
+        night: '#0e0e0e',
+        darkGray: '#30303B'
+      }
     },
   },
   plugins: [],
