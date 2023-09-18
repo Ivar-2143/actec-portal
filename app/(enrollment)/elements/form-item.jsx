6ec23d31414d50a4ddf97bcd79f9fb8eb832c33a
Ivar <jeluzadrian07@gmail.com>
2023-09-18 13:46:15 +0800

    label is changed from 'uppercase' to 'capitalize'

--- app/(enrollment)/elements/form-item.jsx
+++ app/(enrollment)/elements/form-item.jsx
@@ -1,14 +1,14 @@
 import { FormItem, FormField, FormLabel, FormControl, FormDescription, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 
-export default function FormElement({control, fieldName, hint, desc=''}) {
+export default function FormElement({control, fieldName, hint, desc='', style='w-full'}) {
   return (
     <FormField
           control={control}
           name={fieldName}
           render={({ field }) => (
-            <FormItem>
-                <FormLabel className='uppercase'>{fieldName}</FormLabel>
+            <FormItem className={style}>
+                <FormLabel className='capitalize'>{fieldName}</FormLabel>
                 <FormControl>
                     <Input placeholder={hint} {...field} />
                 </FormControl>
