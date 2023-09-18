5f63649a42543d4c4b43780bb26c0664099a5279
Ivar <jeluzadrian07@gmail.com>
2023-09-18 13:56:04 +0800

    adjusted the caption and caption label styling.
    label is not set to hidden and added a text size and color to caption.

--- components/ui/calendar.jsx
+++ components/ui/calendar.jsx
@@ -10,23 +10,25 @@ function Calendar({
   className,
   classNames,
   showOutsideDays = true,
+  captionLayout = "dropdown-buttons",
   ...props
 }) {
   return (
     (<DayPicker
       showOutsideDays={showOutsideDays}
+      captionLayout={captionLayout}
       className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
-        caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption: "flex justify-center pt-1 relative items-center text-sm text-darkGray",
+        caption_label: "hidden text-sm font-medium",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
         ),
-        nav_button_previous: "absolute left-1",
+        nav_button_previous: "absolute left-1", 
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
