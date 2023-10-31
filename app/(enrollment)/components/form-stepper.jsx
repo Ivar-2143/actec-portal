b760c6ffe597ceb82b3e476a89a9f967951b24c5
Ivar-2143 <jeluzadrian07@gmail.com>
2023-10-31 09:32:48 +0800

    Added Stepper (Desktop) Added progress in Tablet Steppers

--- app/(enrollment)/components/form-stepper.jsx
+++ app/(enrollment)/components/form-stepper.jsx
@@ -1,7 +1,9 @@
 
+
 import { Progress } from '@/components/ui/progress'
-import StepItem from './ui/step-item';
+import StepItem, { Step } from './ui/step-item';
 import { PageStepTitles } from '@/lib/form-data';
+import { RightArrowIcon } from '@/public/assets/icons';
 
 export default function FormStepper({page}) {
 
@@ -9,18 +11,48 @@ export default function FormStepper({page}) {
   return (
     <nav className='relative mb-4   bg-white
     sm:h-[144px] sm:my-4 sm:mx-6 sm:px-4 sm:max-lg:rounded-md sm:overflow-hidden
-    lg:left-0 lg:top-0 lg:mx-0 lg:my-0 lg:w-[480px] lg:max-w-[640px] sm:min-h-[144px] '>
+    lg:left-0 lg:top-0 lg:mx-0 lg:my-0 lg:w-[480px] lg:min-w-[300px] lg:h-screen sm:min-h-[144px] '>
+      {/* Desktops */}
+      <div className='hidden lg:flex flex-col relative h-full w-full px-6 '>
+        <h2 className="text-primary text-4xl font-extrabold my-4">
+          ACTEC
+        </h2>
+        <div className='flex flex-col mt-[72px] gap-10'>
+          <Step title='Student Type' step={1} page={page} />
+          <Step title='Program' step={2} page={page} />
+          <Step title='Personal Details' step={3} page={page}>
+            <div className={`flex justify-between ${page == 3? '' : ''}`}>
+              <span className={`${page == 3? '' : 'text-gray'} font-medium`}>About You</span>
+              {page == 3 && (<RightArrowIcon className='text-gray text-lg'/>)}
+            </div>
+            <div className={`flex justify-between ${page == 4? '' : ''}`}>
+              <span className={`${page == 4? '' : 'text-gray'} font-medium`}>Address</span>
+              {page == 4 && (<RightArrowIcon className='text-gray text-lg'/>)}
+            </div>
+            <div className={`flex justify-between ${page == 5? '' : ''}`}>
+              <span className={`${page == 5? '' : 'text-gray'} font-medium`}>Contact Information</span>
+              {page == 5 && (<RightArrowIcon className='text-gray text-lg'/>)}
+            </div>
+            <div className={`flex justify-between ${page == 6? '' : ''}`}>
+              <span className={`${page == 6? '' : 'text-gray'} font-medium`}>Education</span>
+              {page == 6 && (<RightArrowIcon className='text-gray text-lg'/>)}
+            </div>
+          </Step>
+          <Step title='Documents' step={4} page={(page-3)} />
+          <Step title='Summary' step={5} page={(page-3)} />
+          <Step title='Finish' step={6} page={(page-3)} />
+        </div>
+        
+      </div>
+      <svg className='invisible lg:visible absolute bottom-0 left-0 h-[175px]' viewBox="0 0 479 175" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8.14823e-06 73.7891C24.7309 41.2219 64.0803 34.4014 64.0803 34.4014C97.0581 27.2248 132.433 69.3024 149.521 96.7242C166.609 124.146 182.095 70.2995 214.669 59.3308C247.243 48.3621 256.855 112.679 326.275 122.152C395.696 131.625 479 0 479 0V160.044H8.14823e-06V73.7891Z" fill="#D8E8FC"/>
+        <path d="M0 88.7455C24.7309 56.1783 64.0803 49.3578 64.0803 49.3578C97.0581 42.1811 132.433 84.2588 149.521 111.681C166.609 139.102 182.095 85.2559 214.669 74.2872C247.243 63.3185 256.855 127.635 326.275 137.108C395.696 146.581 479 14.9564 479 14.9564V175H0V88.7455Z" fill="#95C1F8"/>
+        </svg>
       {/* Tablets */}
       <div className='hidden sm:max-lg:flex gap-2 w-full overflow-hidden'>
-        <StepItem title='Student Type' step={1}/>
-        <StepItem title='Program' step={2}/>
-        <StepItem title='About You' step={3}/>
-        <StepItem title='Address' step={4}/>
-        <StepItem title='Contact Info' step={5}/>
-        {/* <StepItem title='Education' />
-        <StepItem title='Documents' />
-        <StepItem title='Summary' />
-        <StepItem title='Create Account' /> */}
+        {PageStepTitles.map((title, index) => (
+          <StepItem key={title} title={title} step={(index+1)} page={page}/>
+        ))}
       </div>
 
       {/* mobile */}
--- app/(enrollment)/components/ui/step-item.jsx
+++ app/(enrollment)/components/ui/step-item.jsx
@@ -1,26 +1,76 @@
 import { Badge } from '@/components/ui/badge'
-import React from 'react'
+import { CheckIcon, RightArrowIcon } from '@/public/assets/icons'
 
-export default function StepItem({title, step}) {
-  
+export default function StepItem({title, step, page=null}) {
+  const active = (page == step);
+  const completed = (page > step);
+
   return (
-    <div className='flex flex-row gap-4 items-center 
+    <div className={`flex flex-row gap-4 items-center 
                 my-auto mx-6
                 sm:flex-col sm:w-[136px] sm:min-w-[136px] sm:mx-1 sm:gap-2 sm:items-start
                 sm:my-2
-                '>
+                ${(page - step) > 2? 'hidden' : ''}`}>
         <div className='flex sm:w-full gap-2 justify-start items-center'>
-            <div className=" bg-company w-[56px] h-[56px] rounded-full
+            <div className={`bg-company ${active? 'sm:bg-company': completed? 'sm:bg-betterGreen' : 'sm:bg-gray'} w-[56px] h-[56px] rounded-full
                 sm:w-[32px] sm:h-[32px]
-            " />
-            <div className='invisible w-0 sm:visible bg-company sm:w-[calc(100%-40px)] sm:h-[6px]' />
+            `}/>
+            {/* <div className='invisible w-0 sm:visible bg-company sm:w-[calc(100%-40px)] sm:h-[6px]' /> */}
+            
+            {/* 
+              THIS IS THE LINE
+             */}
+            <div className={`invisible w-0 sm:visible sm:w-[calc(100%-40px)] sm:h-[6px] ${active? 'bg-company': completed? 'bg-betterGreen' : 'bg-gray'}`} />
         </div>
         <div className="step-title 
         flex flex-col text-xl font-medium sm:text-base">
             <span className='text-xs sm:text-[10px] text-gray'>Step {step}/9</span>
             {title}
         </div>
-        <Badge className='invisible sm:visible text-[10px]' variant='default'>Completed</Badge>
+        <Badge className='invisible sm:visible text-[10px]' variant={`${active? 'default': completed? 'green' : 'secondary'}`}>
+          {active? 'In Progress': completed? 'Completed' : 'To Complete'}
+        </Badge>
+    </div>
+  )
+}
+
+export function Step({title, step, page,children}) {
+  if(children != null && page < 7 && page > 2){
+    page = 3
+  }
+  let active = (step == page);
+  const completed = (page > step);
+  
+
+  return(
+    <div className='flex flex-col w-full'>
+        <div className='step-icon flex w-full justify-between items-center'>
+          <div className='flex justify-center items-center gap-2'>
+            <h4 className={`px-2 py-0 rounded-full border-[3px] h-[32px] align-middle font-bold
+              ${
+              active? ' border-company text-company': 
+              completed? ' hidden':  
+              'border-gray text-gray'}
+            `}>{step}</h4>
+            {completed && (
+              <CheckIcon className='text-3xl' />
+            )}
+            <span className={
+              `uppercase font-bold 
+              ${active? 'text-company':
+              completed? 'text-plat':'text-gray'}`
+            }>{title}</span>
+          </div>
+          {/* *Cursor */}
+          {(!children && active) && (
+            <RightArrowIcon className='text-gray text-lg' />
+          )}
+        </div>
+        {(children && active)&& (
+          <div className={`${children? 'ml-10 mt-6 flex flex-col gap-4' : ''}`}>
+            {children}
+          </div>
+        )}
     </div>
   )
 }
