f0bf32a6e70d284853ac54212c7b0bad477269e3
Ivar-2143 <jeluzadrian07@gmail.com>
2023-11-03 22:46:53 +0800

    added calendar widget for dashboards

---
+++ components/shared/dashboard/calendar-widget.jsx
@@ -0,0 +1,30 @@
+import { Calendar } from "@/components/ui/calendar";
+import getDayToString from "@/lib/functions/get-day-to-string";
+import { format } from "date-fns";
+
+
+export default function CalendarWidget() {
+  return (
+    <div className="rounded-md border bg-white flex justify-center ">
+        <Calendar
+        disableNavigation
+            mode="single"
+            className='scale-100 invisible lg:visible'
+            classNames={{
+                caption_label:'block',
+                day_today:'bg-primary text-primary-foreground'
+            }}
+            
+    />
+    </div>
+  )
+}
+
+export function MobileCalendar(){
+  return(
+    <div className="lg:hidden">
+      <h6>Today is {getDayToString(new Date().getDay())},</h6>
+      <h4>{format(new Date(),"PPP")}</h4>
+    </div>
+  )
+}
