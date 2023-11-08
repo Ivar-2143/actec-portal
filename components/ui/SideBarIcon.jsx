92669f19ad891b3b93f6b1711152796b36fdf42d
Ivar-2143 <jeluzadrian07@gmail.com>
2023-11-08 19:13:35 +0800

    Adjusted the active style - now evaluates to active on nested routes

--- components/ui/SideBarIcon.jsx
+++ components/ui/SideBarIcon.jsx
@@ -5,10 +5,16 @@ import { usePathname } from 'next/navigation';
 
 export default function SideBarIcon({icon,label,link}) {
   const pathname = usePathname()
+  let active = false;
 
+  active = pathname === link;
+  if(pathname.lastIndexOf('/') !== 0) {
+    active = pathname.startsWith(link) && link.length > 1
+  }
+  
   return (
     <Link href={link} >
-        <div className={`sidenav-icon ${pathname === link? 'active-link': ''}`}>
+        <div className={`sidenav-icon ${active ? 'active-link': ''}`}>
             {icon} 
             <span className='nav-icon-label'>
               {label}
