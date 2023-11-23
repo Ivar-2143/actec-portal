a16a8553cbb6c69d706c9ff6d0a6914ff6237308
Ivar <jeluzadrian07@gmail.com>
2023-11-23 23:11:15 +0800

    Changes:
    - Refactored the popover so it could be reusable.
    
    - Adjusted the mobile to tablet & desktop style of its elements

--- components/button-sets/registrar/manage-request-button-set.jsx
+++ components/button-sets/registrar/manage-request-button-set.jsx
@@ -1,11 +1,9 @@
 'use client'
 import { Button } from '@/components/ui/button'
-import { Command, CommandGroup, CommandItem, CommandList } from '@/components/ui/command'
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
-import { Separator } from '@/components/ui/separator'
+
 import { toast } from '@/components/ui/use-toast'
 import { ArchiveIcon, CheckIcon, CircleMinusIcon, FilledRemoveIcon } from '@/public/assets/icons'
-import { MoreHorizontal } from 'lucide-react'
+import BulkActionsPopover from '../bulk-actions-popover'
 
 export default function ManageRequestButtons() {
     const options =[{
@@ -37,13 +35,18 @@ export default function ManageRequestButtons() {
     }
 
     return (
-        <>
+        <>  
+            <BulkActionsPopover options={options} onArchive={onArchive}/>
+            <div
+            className='w-9/12
+            max-[370px]:w-full sm:w-auto sm:inline-flex gap-x-2
+            sm:ml-4'>
             <Button
                 variant='default'
                 size='sm'
                 className='
                 rounded-sm w-full
-                sm:rounded-full'
+                sm:rounded-full sm:w-auto'
                 onClick={()=>
                     toast({
                     title: 'Status Changed',
@@ -53,7 +56,7 @@ export default function ManageRequestButtons() {
                 Release Document
             </Button>
             <Button
-                variant='secondary'
+                variant='outline'
                 className='hidden sm:inline-flex rounded-full'
                 size='sm'
                 onClick={()=>
@@ -77,46 +80,9 @@ export default function ManageRequestButtons() {
             >
                 Archive
             </Button>
+        </div>
 
-            <Popover>
-                <PopoverTrigger asChild>
-                    <Button variant="ghost" size="icon" className="h-8 border-dashed">
-                        <MoreHorizontal className="h-4 w-4" />
-                    </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-[200px] p-0" align="start">
-                    <Command>
-                        <CommandList>
-                            <CommandGroup>
-                                {options.map(option => {
-                                    return(
-                                        <CommandItem
-                                            key={option.value}
-                                            onSelect={option.onClick}
-                                        >
-                                        {option.icon && (
-                                            <option.icon className='w-4 h-4 mr-2'  />
-                                        )}
-                                        <span>{option.label}</span>
-                                        {
-                                        //! INSERT DATA COUNT HERE
-                                        }
-                                        </CommandItem>
-                                    )
-                                })}
-                                <Separator className='bg-muted h-px my-1' />
-                                <CommandItem
-                                    onSelect={onArchive}
-                                    className='text-gray'
-                                >
-                                    <ArchiveIcon className='w-4 h-4 mr-2' />
-                                    <span> Archive </span>
-                                </CommandItem>
-                            </CommandGroup>
-                        </CommandList>
-                    </Command>
-                </PopoverContent>
-            </Popover>
+            
         </>
     )
 }
