3e8a69d6ee1585b71fcb8cec9c6fc69addf09aa4
Ivar <jeluzadrian07@gmail.com>
2023-11-27 08:14:59 +0800

    Changes: - Now uses searchParam as a state of each tab.

--- components/table/data-table-tabs.jsx
+++ components/table/data-table-tabs.jsx
@@ -8,41 +8,74 @@ import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Separator } from "../ui/separator";
 import { toast } from "../ui/use-toast";
 import { cn } from "@/lib/utils";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
 
 export function DataTableTabs({title, options,className}){
+    const searchParams = useSearchParams();
+    const data = searchParams.get('data') ?? 'all';
+    const router = useRouter();
+
+    const handleClick = (value) => {
+        router.push(`?${new URLSearchParams({data:value})}`)
+    }
+    
+
+
     return (
-        <Popover>
-            <PopoverTrigger asChild className={cn(className)}>
-                <Button variant="ghost" size="icon" className="h-8 border-dashed">
-                    <FilterIcon className="h-4 w-4" />
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0" align="start">
-                <Command>
-                    <CommandList>
-                        <CommandGroup>
-                            {options.map(option => {
-                                return(
-                                    <CommandItem
-                                        key={option.value}
-                                        onSelect={()=>{
-                                            toast({
-                                                title: 'Switching Table...',
-                                                description: `${option.label} is selected.`
-                                            })
-                                        }}
-                                    >
-                                    <span>{option.label}</span>
-                                    {
-                                      //! INSERT DATA COUNT HERE
-                                    }
-                                    </CommandItem>
-                                )
-                            })}
-                        </CommandGroup>
-                    </CommandList>
-                </Command>
-            </PopoverContent>
-        </Popover>
+        <>
+            <Popover>
+                <PopoverTrigger asChild className={cn('sm:hidden',className)}>
+                    <Button variant="ghost" size="icon" className="h-8 border-dashed">
+                        <FilterIcon className="h-4 w-4" />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] p-0" align="start">
+                    <Command>
+                        <CommandList>
+                            <CommandGroup>
+                                {options.map(option => {
+                                    return(
+                                        <CommandItem
+                                            key={option.value}
+                                            onSelect={()=>{
+                                                toast({
+                                                    title: 'Switching Table...',
+                                                    description: `${option.label} is selected.`
+                                                })
+                                            }}
+                                        >
+                                        <span>{option.label}</span>
+                                        {
+                                          //! INSERT DATA COUNT HERE
+                                        }
+                                        </CommandItem>
+                                    )
+                                })}
+                            </CommandGroup>
+                        </CommandList>
+                    </Command>
+                </PopoverContent>
+            </Popover>
+            <div className='hidden sm:flex w-full border-b border-plat border-collapse'>
+                {options.map(option => (
+                    <div 
+                    key={option.value}
+                    className={cn('py-2 px-4 border-primary', option.value == data && 'border-b-2'  )}
+                    onClick={()=>{
+                        toast({
+                            title: 'Switching Table...',
+                            description: `${option.label} is selected`
+                        })
+                        handleClick(option.value);
+                    }}
+                    >
+                        <span>{option.label}</span>
+                        {
+                            //! INSERT DATA COUNT HERE
+                        }
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
