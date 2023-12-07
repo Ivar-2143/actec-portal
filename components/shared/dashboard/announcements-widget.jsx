100ac2d19f5b75b1e56468dbcbaf339e613473ce
Ivar <jeluzadrian07@gmail.com>
2023-12-07 22:26:32 +0800

    Adjusted styling and height of each announcement. Added className props to adjust the main container.

--- components/shared/dashboard/announcements-widget.jsx
+++ components/shared/dashboard/announcements-widget.jsx
@@ -1,12 +1,14 @@
+import { cn } from '@/lib/utils'
 import React from 'react'
 
-export default function AnnouncementWidget() {
+export default function AnnouncementWidget({className}) {
   return (
-    <div className='w-full relative h-full'>
+    <div className={cn('w-full relative h-full',className)}>
         <h2 className='text-lg font-medium'> Announcements </h2>
-        <div className='flex flex-col h-[calc(100%-28px)] gap-2 w-full'>
-          <div className='bg-company w-full rounded-md h-1/2' />
-          <div className='bg-white w-full rounded-md h-1/2' />
+        <div className='flex flex-col h-[calc(100%-28px)] gap-2 w-full mt-1'>
+          <div className='bg-company w-full rounded-md lg:h-[136px]' />
+          <div className='bg-white w-full rounded-md lg:h-[136px]' />
+          <div className='bg-white w-full rounded-md lg:h-[136px]' />
         </div>
 
     </div>
