1b5a7a667d7ab9e2b55c70ee83654b190ebc5d4c
Ivar <jeluzadrian07@gmail.com>
2023-12-07 22:27:13 +0800

    Added more styling in the Calendar component.
    It is to adjust its scaling in size

--- components/shared/dashboard/calendar-widget.jsx
+++ components/shared/dashboard/calendar-widget.jsx
@@ -1,3 +1,4 @@
+import { buttonVariants } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import getDayToString from "@/lib/functions/get-day-to-string";
 import { cn } from "@/lib/utils";
@@ -6,14 +7,22 @@ import { format } from "date-fns";
 
 export default function CalendarWidget({className}) {
   return (
-    <div className={cn("rounded-lg border bg-white flex justify-center", className)}>
+    <div className={cn("rounded-lg border bg-white flex justify-center py-2 px-2", className)}>
         <Calendar
         disableNavigation
             mode="single"
-            className='scale-100 invisible lg:visible'
+            className='scale-100 invisible lg:visible w-full'
             classNames={{
                 caption_label:'block',
-                day_today:'bg-primary text-primary-foreground'
+                day_today:'bg-primary text-primary-foreground',
+                row: 'flex w-full mt-1 justify-between',
+                cell: 'h-8 w-8 text-xl',
+                day: cn(buttonVariants({variant: 'ghost'}),
+                '"h-8 w-8 p-0 text-base font-normal aria-selected:opacity-100"'),
+                head_cell: 'w-8',
+                head_row: 'flex justify-between',
+                caption: 'text-xl flex justify-center font-bold',
+                months: 'w-full'
             }}
             
     />
