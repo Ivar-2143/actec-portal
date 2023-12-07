b16e4ed83f37351cf6fff4ea8ef5427ce5e83993
Ivar <jeluzadrian07@gmail.com>
2023-12-07 22:28:15 +0800

    Adjusted the width size in lg: devices

--- components/shared/dashboard/dashboard-right-content.jsx
+++ components/shared/dashboard/dashboard-right-content.jsx
@@ -4,7 +4,7 @@ import CalendarWidget from "./calendar-widget";
 
 export default function DashboardRightContent() {
   return (
-    <div className='content-right flex flex-col w-[300px] gap-4 h-full' >
+    <div className='content-right flex flex-col lg:w-[392px] gap-4 h-full' >
         <CalendarWidget />
         <AnnouncementWidget />
       </div>
